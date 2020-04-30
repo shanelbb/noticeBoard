7d0068792986d8d6401d0f7618234370f9b5a3ea
Shanel Beebe <shanelbeebe@icloud.com>
2020-04-30 16:55:35 -0400

    Updates

---
+++ precache-manifest.36b6450e5c0c2169d7b7247fa47d79b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "590cc3b2c26fbe479557ebee28b5e944",
+    "url": "/noticeBoard/index.html"
+  },
+  {
+    "revision": "0359ab2873573af41a38",
+    "url": "/noticeBoard/static/css/main.2ff83cee.chunk.css"
+  },
+  {
+    "revision": "2b06bdb0a459efff75a7",
+    "url": "/noticeBoard/static/js/2.8c2d155a.chunk.js"
+  },
+  {
+    "revision": "4ae2e24854201e2c90f766e681fee893",
+    "url": "/noticeBoard/static/js/2.8c2d155a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0359ab2873573af41a38",
+    "url": "/noticeBoard/static/js/main.675ef4a6.chunk.js"
+  },
+  {
+    "revision": "093487c47677b3a94624",
+    "url": "/noticeBoard/static/js/runtime-main.d311a2e4.js"
+  },
+  {
+    "revision": "5cc01f50bf50dfa734a36548198284bc",
+    "url": "/noticeBoard/static/media/headerIllustration.5cc01f50.png"
+  }
+]);
