9b2b30eff05eebffaa300759cb1d8063f4fddfbe
Shanel Beebe <shanelbeebe@icloud.com>
2020-05-01 00:20:03 -0400

    changed likes functionality so user can only like once

--- src/Likes.js
+++ src/Likes.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
 
 class Likes extends Component {
+  constructor() {
+    super();
+    this.state = {
+      liked: false,
+    };
+  }
   render() {
     return (
       <div className="likesContainer">
-        <button className="likes" onClick={() =>  this.props.likeButton(this.props.noticeId) } tabIndex='0'>
-          <span aria-label="heart emoji like button" role='img'>❤️</span> {this.props.likes}
+        <button
+          className="likes"
+          onClick={() => {
+            this.props.likeButton(this.props.noticeId);
+            // toggles liked state to true which disables the button so user can't like a post more than once.
+            this.setState({ liked: true });
+          }}
+          disabled={this.state.liked}
+          tabIndex="0"
+        >
+          <span aria-label="heart emoji like button" role="img">
+            ❤️
+          </span>{" "}
+          {this.props.likes}
         </button>
       </div>
     );
